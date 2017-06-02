72322b1087568d156cf2c5929b68f62e3ddc45b5
Alexandru Tudose <alexandru.tudose@info.uaic.ro>
2017-06-02 11:48:40 +0300

    Drop python code and restructure the application.
    Eliminated the python code and created a Java core start application to
    serve our static web content.

--- asciipic/cli/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Commands for the project.
-"""
-
-from asciipic.cli.commands import server
-from asciipic.cli.commands import worker
-
-Server = server.Server
-Worker = worker.Worker
--- asciipic/db/oracle/init.py
+++
@@ -1,8 +0,0 @@
-"""OracleDB Mappings."""
-from asciipic.db.oracle import manager
-from asciipic.db.oracle import factory
-
-ORACLE_DB = manager.OracleDBManager()
-
-for table in factory.TableFactory.get_items():
-    ORACLE_DB.register(table)
