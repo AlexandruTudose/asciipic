36dfe2cfaf6c0e5f7da5c1022400db2bbfbcd794
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-06-06 22:19:44 +0300

    Added dtos for search API

---
+++ asciipicSearch/src/main/java/com/asciipic/search/dtos/ResponseDTO.java
@@ -0,0 +1,58 @@
+package com.asciipic.search.dtos;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseDTO implements Serializable {
+
+    private Metadata metadata;
+    private List<String> content;
+
+    public ResponseDTO(Boolean successful, List<String> urls) {
+        this.metadata = new Metadata(successful);
+        this.content = urls;
+    }
+
+    public ResponseDTO(Boolean successful, String string) {
+        this.metadata = new Metadata(successful);
+        this.content = new ArrayList<>();
+        this.content.add(string);
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+
+    public void setContent(List<String> content) {
+        this.content = content;
+    }
+
+    private class Metadata implements Serializable {
+        private Boolean successful;
+
+        Metadata(Boolean successful) {
+            this.successful = successful;
+        }
+
+        public Boolean getSuccessful() {
+            return successful;
+        }
+
+        public void setSuccessful(Boolean successful) {
+            this.successful = successful;
+        }
+    }
+
+
+}
+
