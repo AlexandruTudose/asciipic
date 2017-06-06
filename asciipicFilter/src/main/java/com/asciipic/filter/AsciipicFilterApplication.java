b65763085927ab6fe827de6bf1d316cddc3eb691
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-06-06 23:52:48 +0300

    Added main structure for filter API

---
+++ asciipicFilter/src/main/java/com/asciipic/filter/AsciipicFilterApplication.java
@@ -0,0 +1,12 @@
+package com.asciipic.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciipicFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciipicFilterApplication.class, args);
+	}
+}
