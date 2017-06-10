5c43ec9cde9035fec6085512d9c087d7b5534d41
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-06-10 17:36:40 +0300

    Updated main structure for filter module

--- asciipicFilter/src/main/java/com/asciipic/filter/AsciipicFilterApplication.java
+++ asciipicFilter/src/main/java/com/asciipic/filter/AsciipicFilterApplication.java
@@ -2,8 +2,10 @@ package com.asciipic.filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AsciipicFilterApplication {
 
 	public static void main(String[] args) {
