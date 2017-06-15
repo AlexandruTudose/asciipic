bb1351a62ef8e1cb0c752083dace5387a577f506
Alexandru Tudose <alexandru.tudose@info.uaic.ro>
2017-06-16 02:13:08 +0300

    Implemented empty export

---
+++ asciipic/src/main/java/com/asciipic/commands/ExportCommand.java
@@ -0,0 +1,13 @@
+package com.asciipic.commands;
+
+import com.asciipic.commands.basic.InOutCommand;
+import com.asciipic.dtos.ResponseDTO;
+
+import java.util.List;
+
+public class ExportCommand implements InOutCommand {
+    @Override
+    public ResponseDTO execute(List<String> inputStrings) {
+        return null;
+    }
+}
