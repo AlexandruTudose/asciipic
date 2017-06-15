958f5433a3ffc1230ab12b25f6aa2f2f02ec844d
Alexandru Tudose <alexandru.tudose@info.uaic.ro>
2017-06-16 02:39:21 +0300

    Removed duplicated services.

--- asciipicCrawl/src/main/java/com/asciipic/crawl/services/application/JobService.java
+++
@@ -1,9 +0,0 @@
-package com.asciipic.crawl.services.application;
-
-import com.asciipic.crawl.models.Job;
-import com.asciipic.crawl.services.basic.ReadService;
-import com.asciipic.crawl.services.basic.UpdateService;
-
-
-public interface JobService extends ReadService<Job>, UpdateService<Job> {
-}
--- asciipicCrawl/src/main/java/com/asciipic/crawl/services/basic/UpdateService.java
+++
@@ -1,5 +0,0 @@
-package com.asciipic.crawl.services.basic;
-
-public interface UpdateService<T> {
-    T save(T entity);
-}
