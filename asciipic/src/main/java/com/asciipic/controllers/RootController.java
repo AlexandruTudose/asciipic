8450ee885ab948aca0c1111945c1bbd8e4419858
Alexandru Tudose <alexandru.tudose@info.uaic.ro>
2017-06-16 02:08:58 +0300

    Updated controller to use the new service structure.

--- asciipic/src/main/java/com/asciipic/controllers/RootController.java
+++ asciipic/src/main/java/com/asciipic/controllers/RootController.java
@@ -1,8 +1,8 @@
 package com.asciipic.controllers;
 
-import com.asciipic.dtos.GenericResponseDTO;
+import com.asciipic.dtos.ResponseDTO;
 import com.asciipic.dtos.TerminalPostDTO;
-import com.asciipic.services.application.ParserService;
+import com.asciipic.services.CommandInterpreter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
 public class RootController {
 
     @Autowired
-    ParserService parserService;
+    CommandInterpreter commandInterpreter;
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<GenericResponseDTO> post(@RequestBody TerminalPostDTO terminalPostDTO) {
+    public ResponseEntity<List<ResponseDTO>> post(@RequestBody TerminalPostDTO terminalPostDTO) {
         System.out.println(terminalPostDTO.getCommand());
-        return new ResponseEntity<>(parserService.parse(terminalPostDTO.getCommand()), HttpStatus.OK);
+        return new ResponseEntity<>(commandInterpreter.execute(terminalPostDTO.getCommand()), HttpStatus.OK);
     }
 }
