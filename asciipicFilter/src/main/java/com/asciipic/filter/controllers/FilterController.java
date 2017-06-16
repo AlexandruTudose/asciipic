491ffda5b889405bcaaa468dbd2ff275554c3f88
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-06-16 10:06:38 +0300

    Changed http messages

--- asciipicFilter/src/main/java/com/asciipic/filter/controllers/FilterController.java
+++ asciipicFilter/src/main/java/com/asciipic/filter/controllers/FilterController.java
@@ -4,6 +4,7 @@ import com.asciipic.filter.dtos.ImagePostDTO;
 import com.asciipic.filter.dtos.ResponseDTO;
 import com.asciipic.filter.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,22 +28,31 @@ public class FilterController {
     @Autowired
     private ImageService imageService;
 
+    @Value("${path.domain}")
+    private String domain;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${search.port}")
+    private String searchPort;
+
     @RequestMapping(value = "/filter/resize", method = RequestMethod.POST)
     public ResponseEntity<ResponseDTO> resizeImage(@RequestBody ImagePostDTO imagePostDTO,
-                                                   @RequestParam(value = "height", required = false) Long height,
-                                                   @RequestParam(value = "width", required = false) Long width) {
+            @RequestParam(value = "height", required = false) Long height,
+            @RequestParam(value = "width", required = false) Long width) {
         height = verifyParameter(height);
         width = verifyParameter(width);
 
-        if (height < 0 || width < 0 ) {
-            return new ResponseEntity<>(new ResponseDTO(false, "The arguments must be grater then 0"), HttpStatus.BAD_REQUEST);
+        if (height < 0 || width < 0) {
+            return new ResponseEntity<>(new ResponseDTO(false, "The arguments must be grater then 0"), HttpStatus.OK);
         }
 
         ImagePostDTO newImage;
         try {
             newImage = imageService.resize(imagePostDTO, height, width);
         } catch (Exception e) {
-            return new ResponseEntity<>(new ResponseDTO(false, "Error!"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO(false, "Error!"), HttpStatus.OK);
         }
 
         ResponseDTO newResponse = addImage(newImage);
@@ -51,32 +61,32 @@ public class FilterController {
 
     @RequestMapping(value = "/filter/crop", method = RequestMethod.POST)
     public ResponseEntity<ResponseDTO> cropImage(@RequestBody ImagePostDTO imagePostDTO,
-                                                 @RequestParam(value = "top", required = false) Long top,
-                                                 @RequestParam(value = "bottom", required = false) Long bottom,
-                                                 @RequestParam(value = "left", required = false) Long left,
-                                                 @RequestParam(value = "right", required = false) Long right) {
+            @RequestParam(value = "top", required = false) Long top,
+            @RequestParam(value = "bottom", required = false) Long bottom,
+            @RequestParam(value = "left", required = false) Long left,
+            @RequestParam(value = "right", required = false) Long right) {
         top = verifyParameter(top);
         bottom = verifyParameter(bottom);
         left = verifyParameter(left);
         right = verifyParameter(right);
 
-        if(top + bottom >= imagePostDTO.getHeight() || left + right >= imagePostDTO.getWidth()){
-            return new ResponseEntity<>(new ResponseDTO(false, "The arguments are too big"), HttpStatus.BAD_REQUEST);
+        if (top + bottom >= imagePostDTO.getHeight() || left + right >= imagePostDTO.getWidth()) {
+            return new ResponseEntity<>(new ResponseDTO(false, "The arguments are too big"), HttpStatus.OK);
         }
 
         if (top < 0 || bottom < 0 || left < 0 || right < 0) {
-            return new ResponseEntity<>(new ResponseDTO(false, "The arguments must be grater then 0"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ResponseDTO(false, "The arguments must be grater then 0"), HttpStatus.OK);
         }
 
-        if(top == 0 && bottom == 0 && left == 0 && right ==0){
-            return new ResponseEntity<>(new ResponseDTO(false, "Must have at least one argument"), HttpStatus.BAD_REQUEST);
+        if (top == 0 && bottom == 0 && left == 0 && right == 0) {
+            return new ResponseEntity<>(new ResponseDTO(false, "Must have at least one argument"), HttpStatus.OK);
         }
 
         ImagePostDTO newImage;
         try {
             newImage = imageService.crop(imagePostDTO, top, bottom, left, right);
         } catch (Exception e) {
-            return new ResponseEntity<>(new ResponseDTO(false, "Error!"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO(false, "Error!"), HttpStatus.OK);
         }
 
         ResponseDTO newResponse = addImage(newImage);
@@ -85,7 +95,7 @@ public class FilterController {
 
 
     private ResponseDTO addImage(ImagePostDTO imagePostDTO) {
-        final String uri = "http://localhost:8085/images";
+        final String uri = domain + ":" + searchPort + "/images";
 
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject(uri, imagePostDTO, ResponseDTO.class);
